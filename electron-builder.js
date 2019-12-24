14fdc2dce8b48e8e4531c2df5e72f6fc2e23fe04
AFTM7512 <jMethods@163.com>
2019-12-24 21:20:06 +0800

    add-配置 travis.sh

--- electron-builder.js
+++ electron-builder.js
@@ -1,19 +1,22 @@
 const builder = require('electron-builder');
 const pkg = require('./package.json');
 
+const output = process.platform === 'darwin' ? `package-${process.env.CHANNEL}` : `package-${process.env.ARCH}-${process.env.CHANNEL}`;
+console.log(output)
+
 builder.build({
   config: {
     appId: 'release.tingkelai.electron',
-    productName: '听客来',
+    productName: 'tingkelai',
     electronVersion: pkg.electronVersion,
     directories: {
       output: output,
-      app: 'dir'
+      // app: 'dir'
     },
     publish: {
-      // provider: 'generic',
-      // url: 'https://download.timetask.cn/pc-autoupdate/${os}/${env.CHANNEL}',
-      // channel: '${env.CHANNEL}'
+      provider: 'generic',
+      url: 'https://download.timetask.cn/pc-autoupdate/${os}/${env.CHANNEL}',
+      channel: '${env.CHANNEL}'
     },
     mac: {
       category: 'public.app-category.productivity', //放到生产效率类
@@ -21,8 +24,8 @@ builder.build({
       type: 'distribution'
     },
     dmg: {
-      artifactName: 'rishiqing-mac-${env.CHANNEL}-${version}.${ext}',
-      title: '日事清 ${version}',
+      artifactName: 'tingkelai-mac-${env.CHANNEL}-${version}.${ext}',
+      title: 'tingkelai ${version}',
       contents: [
         {
           x: 30,
@@ -51,16 +54,15 @@ builder.build({
       }
     },
     nsis: {
-      artifactName: '听客来-win-${env.ARCH}-${env.CHANNEL}-${version}.exe',
+      artifactName: 'tingkelai-win-${env.ARCH}-${env.CHANNEL}-${version}.exe',
       shortcutName: '听客来',
       uninstallDisplayName: '听客来 ${version}',
-      // guid: 'F4BC9A4A-E09B-465E-BC10-A8921C46E672',
+      guid: 'BE407C3D-E86D-7273-36F9-69C6E8F9F216',
       installerIcon: "assets/tkl.ico",
       uninstallerIcon: "assets/tkl.ico",
       installerHeaderIcon: "assets/tkl.ico",
       createDesktopShortcut: true,
       createStartMenuShortcut: true,
-      shortcutName: "听客来",
       allowToChangeInstallationDirectory: true,
       oneClick: false,
       perMachine: false
