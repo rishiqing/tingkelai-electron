1908c50f31169601326989d4b32dabc24128ab99
AZ <41629560+AFTM7512@users.noreply.github.com>
2019-12-27 15:15:07 +0800

    log--增加自动更新 (#6)
    * fix-调整 electron 打包入口文件 (#4)
    
    * fix-增加自动更新
    
    * fix-beta v1.0.1

--- electron-builder.js
+++ electron-builder.js
@@ -1,19 +1,29 @@
 const builder = require('electron-builder');
 const pkg = require('./package.json');
 
+const output = process.platform === 'darwin' ? `package-${process.env.CHANNEL}` : `package-${process.env.ARCH}-${process.env.CHANNEL}`;
+
 builder.build({
   config: {
     appId: 'release.tingkelai.electron',
-    productName: '听客来',
+    productName: 'tingkelai',
     electronVersion: pkg.electronVersion,
     directories: {
       output: output,
-      app: 'dir'
+      // app: 'src',  // electron 默认打包的文件夹入口
     },
+    files: [
+      'src/**',
+      'node_modules/**/*',
+      'package.json',
+      'electron-builder.js',
+      'main.js',
+      // 'travis.sh',
+    ],
     publish: {
-      // provider: 'generic',
-      // url: 'https://download.timetask.cn/pc-autoupdate/${os}/${env.CHANNEL}',
-      // channel: '${env.CHANNEL}'
+      provider: 'generic',
+      url: 'https://download.tingkelai.com/pc-autoupdate/${os}/${env.CHANNEL}',
+      channel: '${env.CHANNEL}'
     },
     mac: {
       category: 'public.app-category.productivity', //放到生产效率类
@@ -21,8 +31,8 @@ builder.build({
       type: 'distribution'
     },
     dmg: {
-      artifactName: 'rishiqing-mac-${env.CHANNEL}-${version}.${ext}',
-      title: '日事清 ${version}',
+      artifactName: 'tingkelai-mac-${env.CHANNEL}-${version}.${ext}',
+      title: 'tingkelai ${version}',
       contents: [
         {
           x: 30,
@@ -43,24 +53,23 @@ builder.build({
           arch: [process.env.ARCH]
         }
       ],
-      icon: 'assets/tkl.ico',
+      icon: 'src/assets/tkl.ico',
       publish: {
         provider: 'generic',
-        url: 'https://download.timetask.cn/pc-autoupdate/${os}/${env.ARCH}/${env.CHANNEL}',
+        url: 'https://download.tingkelai.com/pc-autoupdate/${os}/${env.ARCH}/${env.CHANNEL}',
         channel: '${env.CHANNEL}'
       }
     },
     nsis: {
-      artifactName: '听客来-win-${env.ARCH}-${env.CHANNEL}-${version}.exe',
+      artifactName: 'tingkelai-win-${env.ARCH}-${env.CHANNEL}-${version}.exe',
       shortcutName: '听客来',
       uninstallDisplayName: '听客来 ${version}',
-      // guid: 'F4BC9A4A-E09B-465E-BC10-A8921C46E672',
-      installerIcon: "assets/tkl.ico",
-      uninstallerIcon: "assets/tkl.ico",
-      installerHeaderIcon: "assets/tkl.ico",
+      guid: 'BE407C3D-E86D-7273-36F9-69C6E8F9F216',
+      installerIcon: "src/assets/tkl.ico",
+      uninstallerIcon: "src/assets/tkl.ico",
+      installerHeaderIcon: "src/assets/tkl.ico",
       createDesktopShortcut: true,
       createStartMenuShortcut: true,
-      shortcutName: "听客来",
       allowToChangeInstallationDirectory: true,
       oneClick: false,
       perMachine: false
