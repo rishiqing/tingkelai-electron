78703bf3eb8529b4266393f1445075c56e935a32
AZ <41629560+AFTM7512@users.noreply.github.com>
2020-01-13 14:57:59 +0800

    log--调试mac打包 (#26)
    * fix-调试 mac 打包
    
    * fix-增加Travis 备注
    
    * fix-update v1.0.12

--- travis_mac.sh
+++ travis_mac.sh
@@ -7,3 +7,5 @@ elif echo $TRAVIS_BRANCH | grep "^check-deploy-" > /dev/null; then
 elif echo $TRAVIS_BRANCH | grep "^beta-deploy-" > /dev/null; then
   npm run rdp-mac-beta
 fi;
+
+# 等后期时间充裕，学习 shell 脚本后可以去将两个 .sh 文件整合，可以通过 $TRAVIS_OS_NAME = 'osx' 判断环境
