c8b48d9829d53482dde1ae4643629c331023a7bf
AFTM7512 <jMethods@163.com>
2020-01-15 18:17:29 +0800

    fix-调试mac包

--- travis_win.sh
+++ travis_win.sh
@@ -3,12 +3,12 @@
 if echo $TRAVIS_BRANCH | grep "^master-deploy-" > /dev/null; then
   # npm run rdp-ia32-release
   # npm run rdp-x64-release
-  npm run rdp-release
+  npm run rdp-release # 将 check 中的包复制一份放到 release
 elif echo $TRAVIS_BRANCH | grep "^check-deploy-" > /dev/null; then
   npm run rdp-ia32-check #这个是为了提交各种审核而打的包，包的内容和正式版一样
   npm run rdp-x64-check
 elif echo $TRAVIS_BRANCH | grep "^beta-deploy-" > /dev/null; then
   npm run rdp-x64-beta
   npm run rdp-ia32-beta
-  npm run rdp-release
+  # npm run rdp-release
 fi;
