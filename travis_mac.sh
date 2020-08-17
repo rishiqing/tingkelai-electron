ed1a450df2b09d5835684416acb458f406fd7bb4
TimZhang <zwnash@163.com>
2020-08-17 13:51:16 +0800

    add: 立聪堂版构建脚本

--- travis_mac.sh
+++ travis_mac.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # if echo $TRAVIS_BRANCH | grep "^master-deploy-" > /dev/null; then
+# 执行一遍win版本的复制命令就可以了
 #   npm run rdp-mac-release
 if echo $TRAVIS_BRANCH | grep "^check-deploy-" > /dev/null; then
   npm run rdp-mac-check
 elif echo $TRAVIS_BRANCH | grep "^beta-deploy-" > /dev/null; then
   npm run rdp-mac-beta
+elif echo $TRAVIS_BRANCH | grep "^listentown-deploy-" > /dev/null; then
+  npm run rdp-mac-listentown
 fi;
 
 # 等后期时间充裕，学习 shell 脚本后可以去将两个 .sh 文件整合，可以通过 $TRAVIS_OS_NAME = 'osx' 判断环境
--- travis_win.sh
+++ travis_win.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 if echo $TRAVIS_BRANCH | grep "^master-deploy-" > /dev/null; then
-  # npm run rdp-ia32-release
-  # npm run rdp-x64-release
   npm run rdp-release # 将 check 中的包复制一份放到 release
 elif echo $TRAVIS_BRANCH | grep "^check-deploy-" > /dev/null; then
   npm run rdp-ia32-check #这个是为了提交各种审核而打的包，包的内容和正式版一样
@@ -10,5 +8,7 @@ elif echo $TRAVIS_BRANCH | grep "^check-deploy-" > /dev/null; then
 elif echo $TRAVIS_BRANCH | grep "^beta-deploy-" > /dev/null; then
   npm run rdp-x64-beta
   npm run rdp-ia32-beta
-  # npm run rdp-release
+elif echo $TRAVIS_BRANCH | grep "^listentown-deploy-" > /dev/null; then
+  npm run rdp-x64-listentown
+  npm run rdp-ia32-listentown
 fi;
